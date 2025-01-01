1cd8fc12c7934652377fbad6feb568504ab63d3b
rs2070 <142837363+rs2070@users.noreply.github.com>
2025-01-01 18:31:55 -0500

    Add files via upload

---
+++ client/src/assets/icons.js
@@ -0,0 +1,69 @@
+// Importing all icons as described
+import locationIcon from './location.png';
+import priceDownIcon from './pricedown.png';
+import priceUpIcon from './priceup.png';
+import minBathsIcon from './minbaths.png';
+import maxBathsIcon from './maxbaths.png';
+import minBedsIcon from './minbeds.png';
+import maxBedsIcon from './maxbeds.png';
+import minSqftIcon from './minsqft.png';
+import maxSqftIcon from './maxsqft.png';
+import houseTypesIcon from './housetypes.png';
+import politicalTypeIcon from './politicaltype.png';
+import searchIcon from './search.png';
+import priceIcon from './price.png';
+import floorplanIcon from './floorplan.png';
+import republicanIcon from './republican.png';
+import democratIcon from './democrat.png';
+import lotIcon from './lot.png';
+import condoIcon from './condo.png';
+import apartmentIcon from './apartment.png';
+import townhouseIcon from './townhouse.png';
+import houseIcon from './house.png';
+import manufacturedIcon from './manufactured.png';
+import coOpIcon from './co-op.png';
+import multifamilyIcon from './multifamily.png';
+import defaulthomeIcon from "./defaulthome.png"
+import previousIcon from './previous.png';
+import nextIcon from './next.png';
+import currentIcon from './current.png';
+import fiveYearsIcon from './5years.png';
+import tenYearsIcon from './10years.png';
+import twentyYearsIcon from './20years.png';
+
+// Exporting icons as an object
+const icons = {
+    locationIcon,
+    priceDownIcon,
+    priceUpIcon,
+    minBathsIcon,
+    maxBathsIcon,
+    minBedsIcon,
+    maxBedsIcon,
+    minSqftIcon,
+    maxSqftIcon,
+    houseTypesIcon,
+    politicalTypeIcon,
+    searchIcon,
+    priceIcon,
+    floorplanIcon,
+    republicanIcon,
+    democratIcon,
+    lotIcon,
+    condoIcon,
+    apartmentIcon,
+    townhouseIcon,
+    houseIcon,
+    manufacturedIcon,
+    coOpIcon,
+    multifamilyIcon,
+    defaulthomeIcon,
+    previousIcon,
+    nextIcon,
+    currentIcon,
+    fiveYearsIcon,
+    tenYearsIcon,
+    twentyYearsIcon
+};
+
+export default icons;
